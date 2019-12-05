0e754df9a48569292a098326faca6040b4c44cd8
sevriye <sevrverbay@gmail.com>
2019-12-05 14:26:00 -0500

    'a'

---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+
+    public static void main(String[] args) {
+        String myname = "Sevriye";
+        System.out.println(myname);
+        System.out.println("this is another line");
+    }
+}
